5aac75edbc7b1abd6aca94d122480b857042a7e6
MohamedRezq <mrezq.dev@gmail.com>
2023-08-20 08:37:12 -0700

    Task 6. Size is not grandeur v2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	printf("Size of a char is %zu byte(s)\n", sizeof(char));
-	printf("Size of an integer is %zu byte(s)\n", sizeof(int));
-	printf("Size of a float is %zu byte(s)\n", sizeof(float));
-	printf("Size of a double is %zu byte(s)\n", sizeof(double));
-	printf("Size of a long integer is %zu byte(s)\n", sizeof(long int));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
 
