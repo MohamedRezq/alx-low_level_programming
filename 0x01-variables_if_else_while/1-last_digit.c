dd79cec06815d13101b8ac7833a0544724142941
MohamedRezq <mrezq.dev@gmail.com>
2023-08-22 14:09:21 -0700

    Task 1. The last digit v8

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,13 +11,13 @@ int main(void)
 {
 	{
 		int n;
-		unsigned int last_digit;
+		int last_digit;
 		char *state;
 
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
 
-		last_digit = abs(n % 10);
+		last_digit = n % 10;
 
 		if (last_digit == 0)
 		{
