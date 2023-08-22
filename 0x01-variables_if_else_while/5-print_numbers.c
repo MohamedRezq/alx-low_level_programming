2f83269f9cd015c7f80f30ed3c0a1a1813581d6f
MohamedRezq <mrezq.dev@gmail.com>
2023-08-22 15:13:14 -0700

    ALX TASKS: 5. Numbers v1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+
+	putchar(10);
+
+	return (0);
+}
