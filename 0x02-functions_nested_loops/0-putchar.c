695f36c78178a2f0cd55d2fb5bf23475f8d0b9a7
MohamedRezq <mrezq.dev@gmail.com>
2023-08-24 15:39:37 -0700

    0. _putchar v5

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,8 +11,8 @@ int main(void)
 	int i;
 	char *word = "_putchar";
 
-	for (i = 0; i < strlen(word); i++)
-		_putchar(word[i])
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
 	_putchar('\n');
 
 	return (0);
