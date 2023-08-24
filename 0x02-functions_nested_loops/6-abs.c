0ae16897faa00c34826966781af38e1098ae95f0
MohamedRezq <mrezq.dev@gmail.com>
2023-08-24 16:34:15 -0700

    6-abs.c v1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_abs - Entry point
+ *@n: number to check
+ *Return: abs(n)
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+		return (n)
+	else if (n == 0)
+
+		return (0);
+	else
+		return (-n);
+
+}
